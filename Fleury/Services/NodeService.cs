a1f89e5c6dbd0d82bf68ba9963ff0e5742d2936a
DaniilReller <141248625+DaniilReller@users.noreply.github.com>
2024-02-22 14:21:11 +0500

    Creating node service

---
+++ Fleury/Services/NodeService.cs
@@ -0,0 +1,38 @@
+﻿using Fleury.Models;
+
+public static class NodeService
+{
+    private static List<Node> nodes = new List<Node>(){
+        new Node { Id = 1, X = 362, Y = 280, R = 35, Fill="#1aaee5" },
+        new Node { Id = 2, X = 266, Y = 232, R = 15, Fill="#04dcd2" },
+        new Node { Id = 3, X = 325, Y = 299, R = 35, Fill="#1aaee5" },
+        new Node { Id = 4, X = 474, Y = 123, R = 15, Fill="#04dcd2" },
+        new Node { Id = 5, X = 462, Y = 258, R = 35, Fill="#1aaee5" },
+    };
+
+    public static List<Node> GetAllNodes() => nodes;
+
+    public static void AddNode(Node node)
+    {
+        nodes.Add(node);
+    }
+
+    public static void DeleteNode(Node node)
+    {
+        nodes.Remove(node);
+    }
+
+    public static void ConnectNodes(Node node1, Node node2)
+    {
+        if (node1.Id != node2.Id
+            && !node1.ConnectedTo.Contains(node2.Id)
+            && !node2.ConnectedTo.Contains(node1.Id)
+        )
+        {
+            if (node1.Id < node2.Id)
+                node1.ConnectedTo.Add(node2.Id);
+            else
+                node2.ConnectedTo.Add(node1.Id);
+        }
+    }
+}
