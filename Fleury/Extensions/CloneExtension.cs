1f809a5b1614fca9f0265b9e0b9c2c91b818a5d4
DaniilReller <141248625+DaniilReller@users.noreply.github.com>
2024-02-29 18:35:51 +0500

    CLone method

---
+++ Fleury/Extensions/CloneExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+
+namespace Fleury.Extensions;
+
+public static class CloneExtension
+{
+    public static T Clone<T>(this T? obj)
+    {
+        if (obj == null)
+        {
+            throw new ArgumentNullException(nameof(obj), "Object to clone cannot be null.");
+        }
+        string jsonString = JsonSerializer.Serialize(obj);
+        return JsonSerializer.Deserialize<T>(jsonString)!;
+    }
+}
